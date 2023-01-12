f6866325b3ddfb5c6d217b78a535934a9da4d571
Ahmed Ali <kaber596@gmail.com>
2023-01-12 22:30:34 +0500

    updated

--- src/pages/Compose.jsx
+++ src/pages/Compose.jsx
@@ -3,7 +3,7 @@ import { useGlobalContext } from "../context/Context";
 import axios from "axios";
 import "../style.css";
 
-// const url = 'https://filthy-housecoat-dove.cyclic.app';
+const url = 'https://filthy-housecoat-dove.cyclic.app/api';
 
 export default function Compose() {
       const [title, setTitle] = useState("");
@@ -26,13 +26,13 @@ export default function Compose() {
 			data.append("file", file);
 			newPost.photo = filename;
 			try {
-				await axios.post("/upload", data);
+				await axios.post(url + "/upload", data);
 			} catch (err) {}
 		}
 		try {
-		    const res = await axios.post("/posts", newPost);
+		    const res = await axios.post(url + "/posts", newPost);
                 console.log(res)
-		    window.location.replace("/post/" + res.data._id);
+		    window.location.replace(url + "/post/" + res.data._id);
 		} catch (err) {}
 	};
      
