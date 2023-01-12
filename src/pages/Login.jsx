df97a23f989c32b362e90c1698b0cb7ab7c78844
Ahmed Ali <kaber596@gmail.com>
2023-01-12 22:17:10 +0500

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { Input, Button } from '../components/reuseable components';
 import "../style.css";
 
-const url = 'https://filthy-housecoat-dove.cyclic.app';
+// const url = 'https://filthy-housecoat-dove.cyclic.app';
 
 export default function Login() {
 	const [username, setUsername] = useState("");
@@ -15,7 +15,7 @@ export default function Login() {
 		e.preventDefault();
 		dispatch({ type: "LOGIN_START" });
 		try {
-			const res = await axios.post(url + "/auth/login", {
+			const res = await axios.post("/auth/login", {
 				username: username,
 				password: password
 			});
